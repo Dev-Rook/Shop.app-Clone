36f591a5177dee45aac5953b3e6c768f1a43b8d1
Doc Savage <97268006+DocSavage11@users.noreply.github.com>
2022-05-05 19:14:35 -0500

    working side bar

--- main.js
+++ main.js
@@ -11,3 +11,27 @@ play.onclick = function () {
     play.style.display = "none"
 }
 
+// /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+// function openNav() {
+//     document.getElementById("mySidebar").style.width = "50%";
+//     document.getElementById("main").style.marginright = "250px";
+//   }
+  
+//   /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+//   function closeNav() {
+//     document.getElementById("mySidebar").style.width = "0";
+//     document.getElementById("main").style.marginright = "0";
+//   }
+
+
+function toggle() {
+    const OC = document.getElementById("sideBar");
+
+    if(OC.style.display === "block") {
+        OC.style.display = "none";
+    }
+
+    else {
+        OC.style.display = "block"
+    }
+}
